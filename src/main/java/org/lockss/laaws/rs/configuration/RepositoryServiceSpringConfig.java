1228677ef1092be7f79808b0ad46170a1a1b83b9
Daniel Vargas <dlvargas@stanford.edu>
2020-09-23 01:05:06 -0700

    Configure CommonsMultipartResolver temporary upload direcotry and maximum size in memory before writing to file

--- src/main/java/org/lockss/laaws/rs/configuration/RepositoryServiceSpringConfig.java
+++ src/main/java/org/lockss/laaws/rs/configuration/RepositoryServiceSpringConfig.java
@@ -1,10 +1,15 @@
 package org.lockss.laaws.rs.configuration;
 
+import org.apache.commons.io.FileUtils;
+import org.lockss.util.io.FileUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import java.io.IOException;
 
 /**
  * Spring configuration beans for the Spring-implementation of the LOCKSS Repository Service.
@@ -20,9 +25,15 @@ public class RepositoryServiceSpringConfig {
    * @return A {@link CommonsMultipartResolver} for the Spring-implementation of the LOCKSS Repository Service.
    */
   @Bean
-  public CommonsMultipartResolver multipartResolver() {
+  public CommonsMultipartResolver multipartResolver() throws IOException {
     CommonsMultipartResolver resolver = new CommonsMultipartResolver();
-//    resolver.setMaxInMemorySize((int) (16L * FileUtils.ONE_KB)); // default 10KiB
+
+    // Set the upload temporary directory
+    resolver.setUploadTempDir(new FileSystemResource((FileUtil.createTempDir("multipartResolver", null))));
+
+    // Default is 10KiB which is too small and would cause a lot of temp files to be opened
+    resolver.setMaxInMemorySize((int) (4L * FileUtils.ONE_MB));
+
     return resolver;
   }
 }
