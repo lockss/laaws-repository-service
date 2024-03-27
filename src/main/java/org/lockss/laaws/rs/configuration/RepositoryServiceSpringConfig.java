a34244a0220e62b0fee92d15432291a968599a31
Daniel Vargas <dlvargas@stanford.edu>
2024-03-27 12:36:06 -0700

    Remove redundant errorAttributes bean from RepositoryServiceSpringConfig; move FIXME comment

--- src/main/java/org/lockss/laaws/rs/configuration/RepositoryServiceSpringConfig.java
+++ src/main/java/org/lockss/laaws/rs/configuration/RepositoryServiceSpringConfig.java
@@ -73,20 +73,6 @@ public class RepositoryServiceSpringConfig {
 
   LockssMultipartResolver multipartResolver;
 
-  // FIXME: This was a mistake; revert (and make sure to update our clients)
-  @Bean
-  public DefaultErrorAttributes errorAttributes() {
-    return new DefaultErrorAttributes() {
-      @Override
-      public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-        Map<String, Object> attributes = super.getErrorAttributes(webRequest, options);
-        attributes.remove("timestamp");
-        attributes.put("timestamp", TimeBase.nowMs());
-        return attributes;
-      }
-    };
-  }
-
   @Bean
   public LockssMultipartResolver multipartResolver(ObjectProvider<MultipartProperties> multipartPropsProvider) {
     multipartResolver = new LockssMultipartResolver(multipartPropsProvider.getIfAvailable());
