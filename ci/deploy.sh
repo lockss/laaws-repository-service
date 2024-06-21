d5567eccd4c73d74e933976321039a048147d361
Claire Griffin <clairetg@stanford.edu>
2024-06-21 13:22:17 -0700

    Remove travis CI support

--- ci/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	mvn -P travis-maven-central --settings ci/settings.xml ;
-    mvn -P travis-docker-hub ;
-fi
