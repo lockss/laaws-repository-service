06451f40735ed7666c03ba7bdc90553cabf75890
Tom Lipkis <tal@pss.com>
2025-08-11 20:32:53 -0700

    Log LockssArtifactAlreadyExistsException

--- src/main/java/org/lockss/laaws/rs/impl/ArtifactsApiServiceImpl.java
+++ src/main/java/org/lockss/laaws/rs/impl/ArtifactsApiServiceImpl.java
@@ -275,6 +275,7 @@ public class ArtifactsApiServiceImpl extends BaseSpringApiServiceImpl
 
         return new ResponseEntity<>(artifact, HttpStatus.OK);
       } catch (LockssArtifactAlreadyExistsException e) {
+        log.error("Artifact already exists: {}", e.getArtifactId().toString());
         throw new LockssRestServiceException(
             LockssRestHttpException.ServerErrorType.DATA_ERROR,
             HttpStatus.CONFLICT,
